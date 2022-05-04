2df0cbedd0c5bbb21a06a59a3415fcf4f26f59f9
Kevin Haubris <kevin.haubris@gmail.com>
2022-05-04 08:52:22 -0500

    Added example of linux and mac specific uname calls.

--- SA/src/uname.c
+++ SA/src/uname.c
@@ -6,13 +6,26 @@
 
 #include "beacon_api.h"
 
+struct mac_utsname{
+    char    sysname[256];  /* [XSI] Name of OS */
+    char    nodename[256]; /* [XSI] Name of this network node */
+    char    release[256];  /* [XSI] Release level */
+    char    version[256];  /* [XSI] Version level */
+    char    machine[256];  /* [XSI] Hardware type */
+};
+
 int run_uname(void){
     struct utsname values;
+    struct mac_utsname values_mac;
     int retcode = 0;
     char* osversion = getOSName();
     if (strcmp("lin", osversion)==0){
         retcode = uname(&values);
         BeaconPrintf(CALLBACK_OUTPUT, "%s %s %s %s %s\n", values.sysname, values.nodename, values.release, values.version, values.machine);
+    }
+    else if (strcmp("apple", osversion) == 0){
+        retcode = uname((struct utsname*)&values_mac);
+        BeaconPrintf(CALLBACK_OUTPUT, "%s %s %s %s %s\n", values_mac.sysname, values_mac.nodename, values_mac.release, values_mac.version, values_mac.machine);
     }
     else{
         BeaconPrintf(CALLBACK_OUTPUT, "Not supported\n");
